2b7dd2955ca86ba92132ea4db585a7a94227e113
dkibogy <jepkemboikibogy5@gmail.com>
2020-05-17 11:55:17 +0300

    made a group header to group the order being made using the thread tag

---
+++ javascript/scripts.js
@@ -0,0 +1,22 @@
+// services
+$(document).ready(function(){
+    $("#foodLogo").click(function(){
+        $("#toggleFood").animate({
+            height: "toggle"
+        });
+    });
+});
+$(document).ready(function(){
+    $("#customerLogo").click(function(){
+        $("#toggleCustomer").animate({
+            height: "toggle"
+        });
+    });
+});
+$(document).ready(function(){
+    $("#deliveryLogo").click(function(){
+        $("#toggleDelivery").animate({
+            height: "toggle"
+        });
+    });
+});
