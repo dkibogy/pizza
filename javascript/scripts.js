009444ba3eab51ffde5a043712b69a5803ca1868
dkibogy <jepkemboikibogy5@gmail.com>
2020-05-17 12:52:36 +0300

    defined the variable for the order forms

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -20,3 +20,17 @@ $(document).ready(function(){
         });
     });
 });
+
+// The order form business logic.
+var price , crustprice, toppingprice ;
+let total = 0;
+class orderpizza {
+    constructor(type, size, crust, toppings, total) {
+        this.type = type;
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+        this.total = total;
+    }
+}
+
